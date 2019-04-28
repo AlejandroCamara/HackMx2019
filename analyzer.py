e5e8e961e0a4c162e3b23ab383820743fad9e02e
Servio Tulio Reyes Castillo <servio_trc@yahoo.com.mx>
2019-04-28 03:52:32 -0500

    hola

--- analyzer.py
+++ analyzer.py
@@ -1,34 +1,76 @@
 import ssl, socket
 from datetime import datetime
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
-regex = r""
-POINT_COUNT = 0
+def obtain_countries(soup):
+    countries = []
+    for table in soup:
+        rows = table.findAll("tr")
+        for row in rows:
+            if "country" in row.text.lower():
+                cols = row.findAll("td")
+                countries.append(cols[1].text.lower())
+    return countries
 
-full_web_page = 'http://google.com'
-web_page = full_web_page.replace("http://", "")
-web_page = web_page.replace("https://", "")
-hostname = web_page.split(".")[0].lower()
-ctx = ssl.create_default_context()
-s = ctx.wrap_socket(socket.socket(), server_hostname=web_page)
-s.connect((web_page, 443))
-cert = s.getpeercert()
-print(cert)
-subject = dict(x[0] for x in cert['subject'])
-issued_to = subject['commonName'].lower()
-issuer = dict(x[0] for x in cert['issuer'])
-issued_by = issuer['commonName'].lower()
-issuer = cert['notAfter'][:-4]
-datetime_object = datetime.strptime(issuer, '%m %d %H:%m')
+def find_country(countries, search):
+    for country in countries:
+        if search in country:
+            return True
+    return False
 
+def primer_filtro(url):
+    point_count = 0
+    full_web_page = url
+    web_page = full_web_page.replace("http://", "")
+    web_page = web_page.replace("https://", "")
+    hostname = web_page.split(".")[0].lower()
+    try:
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(socket.socket(), server_hostname=web_page)
+        s.connect((web_page, 443))
+        cert = s.getpeercert()
+    except Exception:
+        return 0, ""
+    subject = dict(x[0] for x in cert['subject'])
+    issued_to = subject['commonName'].lower()
+    issuer = dict(x[0] for x in cert['issuer'])
+    issued_by = issuer['commonName'].lower()
+    issuer = cert['notAfter'][:-4]
+    datetime_cert = datetime.strptime(issuer, '%b %d %H:%M:%S %Y')
+    country = dict(x[0] for x in cert['subject'])
+    full_country = country['countryName'].lower()
 
-if hostname in issued_to:
-    POINT_COUNT += 1
+    if hostname in issued_to:
+        point_count += 1
 
-if not ("Let's Encrypt Authority X3".lower() in issued_by):
-    POINT_COUNT += 1
+    if not ("Let's Encrypt".lower() in issued_by):
+        point_count += 1
 
+    if datetime.now() < datetime_cert:
+        point_count += 1
 
+    return point_count, full_country
 
-print(datetime_object)
-print(issued_by)
-print(hostname)
+def segundo_filtro(url, country):
+    url_full = f"https://check-host.net/ip-info?host={url}"
+    page = Request(url_full, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(page).read()
+    soup = BeautifulSoup(webpage, "html.parser")
+    countries = obtain_countries(soup.findAll("table", {"class": "inside_info"}))
+    return find_country(countries, country)
+
+def main():
+    # url = "http://banortebxi.com-acceso.mxnet24.com/wps/portal/banorte/"
+    url = input("Ingrese el sitio a buscar: ")
+    cuenta, pais = primer_filtro(url)
+    if pais and segundo_filtro(url, pais):
+        cuenta += 1
+    if cuenta == 4:
+        print("SITIO BUENO")
+    elif cuenta in [2, 3]:
+        print("SITIO REVERVADO")
+    else:
+        print("SITIO MALO CUIDADO!!!")
+
+main()
--- fishblocker/popups/analyzer.js
+++ fishblocker/popups/analyzer.js
@@ -1,1 +1,128 @@
-function()
+var sitios_buenos = ['https://www.google.com/', 'https://www.banorte.com']
+var sitios_malos = ['http://banortebxi.com-acceso.mxnet24.com/wps/portal/banorte/']
+
+function displayDanger() {
+    const Url = '';
+    const Data = {
+        url: "http://banortebxi.com-acceso.mxnet24.com/wps/portal/banorte/"
+    };
+    fetch(Url)
+        .then((res) => {
+            myFunction(res)
+        });
+}
+function displayWarning() {
+    const Url = '';
+    const Data = {
+        url: "http://34.212.143.74/apps/s201911/tc3048/noticias/"
+    };
+    fetch(Url)
+        .then((res) => {
+            myFunction(res)
+        });
+}
+function displayOk() {
+    const Url = '';
+    const Data = {
+        url: "https://www.banorte.com/wps/portal/banorte"
+    };
+    fetch(Url)
+        .then((res) => {
+            myFunction(res)
+        });
+}
+function myFunction(tipo) {
+    console.log("Entre a la funcion " + tipo)
+    let pagina = ""
+    let color = ""
+    let url = ""
+    switch (tipo) {
+        case "0":
+            pagina = `
+      <body>
+      <h1 class="text-center" style='color: white'>¡Todo en orden!</h1>
+      <br>
+    <h2 class="text-center" style='color: white'>
+      <b> Puedes navegar sin preocupación alguna :D </b>
+    </h2>
+      <!-- <button onclick='myFunction("1")'>Try it</button>-->
+      </body>`
+            color = "bg-success"
+            break;
+        case "1":
+            pagina = `
+          <body >
+      <h1 class="text-center" style='color: white'>¡Advertencia!</h1>
+      <br>
+    <h2>
+      <div class="text-center" style='color: white'>
+        <p><b>Es posible que este sitio no sea de quien dice ser.</b></p>
+        <p><b>La página no cuenta con un certificado digital vigente.</b></p>
+        <p><b>Contiene enlaces a páginas externas al sitio.</p>
+      </div>
+    </h2>
+      <!--<button onclick='myFunction("2")'>Try it</button>-->
+      </body>`
+            color = "bg-warning"
+            break;
+        case "2":
+            pagina = `
+      <body >
+      <h1 class="text-center" style='color: white'>¡Peligro!</h1>
+      <br>
+      <h2>
+      <div class="text-center" style='color: white'>
+        <p><b>El origen de la página es extranjero.</b></p>
+        <p><b>El certificado digital no es oficial.</b></p>
+        <p><b>La página no encripta los datos que recibe.</p>
+      </div>
+      </h2>
+      <!--<button onclick='myFunction("0")'>Try it</button>-->
+      </body>`
+            color = "bg-danger"
+            break;
+    }
+    document.body.innerHTML = pagina;
+    document.body.className = color
+}
+
+function escoger() {
+    function logTabsForWindows(windowInfoArray) {
+        var url = windowInfoArray[0].tabs[0].url;
+        //document.querySelector("#beast").innerHTML = url;
+        var i;
+        var hecho = false;
+        for (i = 0; i < sitios_buenos.length; i++) {
+            if (url === sitios_buenos[i]) {
+                hecho = true;
+                myFunction("0");
+                break;
+            }
+        }
+        if (!hecho) {
+            for (i = 0; i < sitios_malos.length; i++) {
+                if (url === sitios_malos[i]) {
+                    hecho = true;
+                    myFunction("2");
+                    break;
+                }
+            }
+        }
+        if (!hecho) {
+            myFunction('1');
+            //document.querySelector("#beast").innerHTML = url;
+        }
+    }
+
+    function onError(error) {
+        return error;
+    }
+
+    var getting = browser.windows.getAll({
+        populate: true,
+        windowTypes: ["normal"]
+    });
+
+    getting.then(logTabsForWindows, onError);
+}
+escoger();
